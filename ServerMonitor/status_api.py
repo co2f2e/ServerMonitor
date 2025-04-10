781640759b1be19106eb61bcdb1896b6c8a40486
co2f2e <48397609+co2f2e@users.noreply.github.com>
2025-04-11 07:04:13 +0800

    Update status_api.py

--- ServerMonitor/status_api.py
+++ ServerMonitor/status_api.py
@@ -55,4 +55,4 @@ async def get_status():
     }
 
 if __name__ == "__main__":
-    uvicorn.run("server_status_api:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("status_api:app", host="127.0.0.1", port=8000, reload=True)
